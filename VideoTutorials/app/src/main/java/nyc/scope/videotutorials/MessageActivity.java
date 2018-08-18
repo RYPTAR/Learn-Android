c2a564543ffd3d9559ade53062791afb076410ac
RYPTAR <ray.dej21@gmail.com>
2018-08-18 17:16:06 -0400

    7- sends and sets message from main to message activity, 5- fixed text input field

--- VideoTutorials/app/src/main/java/nyc/scope/videotutorials/MessageActivity.java
+++ VideoTutorials/app/src/main/java/nyc/scope/videotutorials/MessageActivity.java
@@ -1,7 +1,9 @@
 package nyc.scope.videotutorials;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MessageActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+
+        Intent intent = getIntent();
+
+        String message = intent.getStringExtra("EXTRA_MESSAGE");
+
+        TextView textView = findViewById(R.id.display_message);
+        textView.setText(message);
     }
 }
