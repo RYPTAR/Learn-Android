f91c8268fb406dfd9c0f3a549b99f6daf84225ae
RYPTAR <ray.dej21@gmail.com>
2018-08-18 18:15:16 -0400

    8- created and linked button from message to final activity, centered final activity text

--- VideoTutorials/app/src/main/java/nyc/scope/videotutorials/MessageActivity.java
+++ VideoTutorials/app/src/main/java/nyc/scope/videotutorials/MessageActivity.java
@@ -3,6 +3,7 @@ package nyc.scope.videotutorials;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MessageActivity extends AppCompatActivity {
@@ -18,5 +19,10 @@ public class MessageActivity extends AppCompatActivity {
 
         TextView textView = findViewById(R.id.display_message);
         textView.setText(message);
+    }
+
+    public void openFinalActivity(View view) {
+
+        startActivity(new Intent(this, FinalActivity.class));
     }
 }
