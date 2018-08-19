60921a0add8438f369c0c0f06df59f075f8cfcb4
RYPTAR <ray.dej21@gmail.com>
2018-08-18 21:47:55 -0400

    11- Added in home fragment to main activity

--- Fragments/app/src/main/java/nyc/scope/fragments/MainActivity.java
+++ Fragments/app/src/main/java/nyc/scope/fragments/MainActivity.java
@@ -1,5 +1,7 @@
 package nyc.scope.fragments;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -11,11 +13,26 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("native-lib");
     }
 
+    public static FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fragmentManager = getSupportFragmentManager();
+
+        if(findViewById(R.id.fragment_container) != null){
+
+            if(savedInstanceState!=null){
+                return;
+            }
+
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            HomeFragment homeFragment = new HomeFragment();
+            fragmentTransaction.add(R.id.fragment_container, homeFragment, null);
+            fragmentTransaction.commit();
+        }
 
     }
 
