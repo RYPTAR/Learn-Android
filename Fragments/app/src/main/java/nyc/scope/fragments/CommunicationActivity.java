df263ff0bacca8c0e9dc431b0d9cf5cc356b1d18
RYPTAR <ray.dej21@gmail.com>
2018-08-29 14:03:35 -0400

    13- Created communication activity with message fragment

--- Fragments/app/src/main/java/nyc/scope/fragments/CommunicationActivity.java
+++ Fragments/app/src/main/java/nyc/scope/fragments/CommunicationActivity.java
@@ -1,5 +1,6 @@
 package nyc.scope.fragments;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,16 @@ public class CommunicationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_communication);
+
+        if(findViewById(R.id.fragment_container) != null){
+            if(savedInstanceState != null){
+                return;
+            }
+
+            MessageFragment messageFragment = new MessageFragment();
+
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, messageFragment, null);
+            fragmentTransaction.commit();
+        }
     }
 }
