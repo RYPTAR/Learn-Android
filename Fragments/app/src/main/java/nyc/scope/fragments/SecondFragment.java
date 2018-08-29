96337114e53368a48d8613b7f5923bb6f620bf2b
RYPTAR <ray.dej21@gmail.com>
2018-08-29 14:41:56 -0400

    Added functionality to open communication activity

--- Fragments/app/src/main/java/nyc/scope/fragments/SecondFragment.java
+++ Fragments/app/src/main/java/nyc/scope/fragments/SecondFragment.java
@@ -1,11 +1,13 @@
 package nyc.scope.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -13,6 +15,7 @@ import android.view.ViewGroup;
  */
 public class SecondFragment extends Fragment {
 
+    Button button;
 
     public SecondFragment() {
         // Required empty public constructor
@@ -23,7 +26,23 @@ public class SecondFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+
+        button = view.findViewById(R.id.b3);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getActivity(), CommunicationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 
+    /*public void openCommunicationActivity(View view){
+        Intent intent = new Intent(getActivity(), CommunicationActivity.class);
+        startActivity(intent);
+    }*/
 }
