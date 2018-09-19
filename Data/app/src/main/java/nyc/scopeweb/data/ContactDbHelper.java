034f2557b1ef20e9b6ca273e60cf2a28ce4a70fb
Ray De Jesus <ray.dej21@gmail.com>
2018-09-18 20:00:49 -0400

    18- Created SQLiteOpenHelper class

---
+++ Data/app/src/main/java/nyc/scopeweb/data/ContactDbHelper.java
@@ -0,0 +1,36 @@
+package nyc.scopeweb.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class ContactDbHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "contact_db";
+    public static final int DATABASE_VERSION = 1;
+
+    public static final String CREATE_TABLE = "create table "+ ContactContract.ContactEntry.TABLE_NAME
+            +"("+ ContactContract.ContactEntry.CONTACT_ID +" number,"
+            + ContactContract.ContactEntry.NAME+" text,"
+            + ContactContract.ContactEntry.EMAIL+" text);";
+    public static final String DROP_TABLE = "drop table if exists "+ ContactContract.ContactEntry.TABLE_NAME;
+
+    public ContactDbHelper(Context context){
+
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        Log.d("Database Operation", "Database created!");
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE);
+        Log.d("Database Operation", "Table created!");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(DROP_TABLE);
+        onCreate(db);
+    }
+}
