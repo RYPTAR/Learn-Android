ea2683621df8d1f357d20d2a0b2a14974fce416a
Ray De Jesus <ray.dej21@gmail.com>
2018-09-28 13:19:53 -0400

    19- Connected add contact functionality to the database

--- Data/app/src/main/java/nyc/scopeweb/data/AddContactFragment.java
+++ Data/app/src/main/java/nyc/scopeweb/data/AddContactFragment.java
@@ -1,11 +1,14 @@
 package nyc.scopeweb.data;
 
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
@@ -13,6 +16,8 @@ import android.view.ViewGroup;
  */
 public class AddContactFragment extends Fragment {
 
+    Button BnSave;
+    EditText Id, Name, Email;
 
     public AddContactFragment() {
         // Required empty public constructor
@@ -23,7 +28,34 @@ public class AddContactFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_contact, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_contact, container, false);
+
+        BnSave = view.findViewById(R.id.bn_save);
+        Id = view.findViewById(R.id.text_contact_id);
+        Name = view.findViewById(R.id.text_contact_name);
+        Email = view.findViewById(R.id.text_contact_email);
+
+        BnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String id = Id.getText().toString();
+                String name = Id.getText().toString();
+                String email = Id.getText().toString();
+
+                ContactDbHelper contactDbHelper = new ContactDbHelper(getActivity());
+                //** WARNING! ** Never actually do a database operation on the main thread
+                SQLiteDatabase db = contactDbHelper.getWritableDatabase();
+                contactDbHelper.addContact(Integer.parseInt(id), name, email, db);
+                contactDbHelper.close();
+
+                Id.setText("");
+                Name.setText("");
+                Email.setText("");
+            }
+        });
+
+        return view;
     }
 
 }
